1091ad3e44256d5127756f5d1779229d1b148a7b
admin@dbsgw.cn <admin@dbsgw.cn>
2022-04-13 21:07:23 +0800

    初始化 beego api

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "dbsgw_rust_server/routers"
+
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
