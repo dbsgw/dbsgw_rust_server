5214002e651880bc972d55f4c6f36d2898e500b1
坤元 <admin@dbsgw.cn>
2022-04-28 07:55:11 +0800

    email 封装

--- go.mod
+++ go.mod
@@ -62,6 +62,8 @@ require (
 	golang.org/x/tools v0.1.9 // indirect
 	golang.org/x/xerrors v0.0.0-20220411194840-2f41105eb62f // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
---
+++ utils/RustEmail/email.go
@@ -0,0 +1,68 @@
+package RustEmail
+
+// 此处配置 没有放到app.conf 是为了 保护个人邮箱安全
+
+import (
+	"fmt"
+	"gopkg.in/gomail.v2"
+)
+
+type RustSendEmail struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+}
+
+const (
+	RustEmailHost     = "smtp.mxhichina.com"
+	RustEmailPort     = 25
+	RustEmailUsername = "admin@dbsgw.cn"
+	RustEmailPassword = "ly8334721.="
+)
+
+func (e RustSendEmail) Send(mailTo []string, subject string, body string) error {
+	m := gomail.NewMessage()
+	// 发件人信息
+	m.SetHeader("From", m.FormatAddress("admin@dbsgw.cn", "Rust中文网"))
+	// 收件人
+	m.SetHeader("To", mailTo...)
+	// 主题
+	m.SetHeader("Subject", subject)
+	// 内容
+	m.SetBody("text/html", body)
+
+	d := gomail.NewDialer(e.Host, e.Port, e.Username, e.Password)
+	err := d.DialAndSend(m)
+	if err != nil {
+		// 处理错误
+		return err
+	}
+	fmt.Println("发送成功")
+	return nil
+}
+
+func NewRustSendEmail(host, username, password string, port int) *RustSendEmail {
+	return &RustSendEmail{
+		Host:     host,
+		Port:     port,
+		Username: username,
+		Password: password,
+	}
+}
+
+// NewDefaultSendEmail 默认配置发送邮件
+func NewDefaultSendEmail() *RustSendEmail {
+	return &RustSendEmail{
+		Host:     RustEmailHost,
+		Port:     RustEmailPort,
+		Username: RustEmailUsername,
+		Password: RustEmailPassword,
+	}
+}
+func init() {
+
+	//rustEmail := NewRustSendEmail(RustEmailHost, RustEmailUsername, RustEmailPassword, RustEmailPort)
+	//rustEmail := NewDefaultSendEmail()
+	//rustEmail.Send([]string{"1578347363@qq.com"}, "Rust中文网", "<h1>来自Rust中文网验证码：654230222222</h1>")
+}
