0cc1cb557a48bad10ccb0bd2748ac87a274eb4b5
admin@dbsgw.cn <admin@dbsgw.cn>
2022-04-27 13:34:43 +0800

    gitee 和githup 授权优化

--- controllers/v1/UserController.go
+++ controllers/v1/UserController.go
@@ -7,8 +7,6 @@ import (
 	"dbsgw_rust_server/utils/RustConstant"
 	"dbsgw_rust_server/utils/RustGitHup"
 	"dbsgw_rust_server/utils/RustGitee"
-	"encoding/json"
-	"fmt"
 	"github.com/beego/beego/v2/client/orm"
 	"github.com/beego/beego/v2/core/validation"
 	gonanoid "github.com/matoous/go-nanoid/v2"
@@ -16,17 +14,12 @@ import (
 	"strconv"
 )
 
-// Operations about Users
+// UserController Operations about Users
 type UserController struct {
 	controllers.BaseController
 }
 
-// @Title 登录
-// @Description Logs user into the system
-// @Param	username		query 	string	true		"The username for login"
-// @Param	password		query 	string	true		"The password for login"
-// @Success 200 {string} login success
-// @Failure 403 user not exist
+// Login  登录
 func (u *UserController) Login() {
 	// 通过登录 source 方式 来判读 是邮箱还是第三方
 	// 邮箱： 通过  Email 和 密码登录
@@ -48,14 +41,11 @@ func (u *UserController) Login() {
 	} else {
 		switch source {
 		case RustConstant.EMAIL:
-			fmt.Println("email登录")
 			break
 		case RustConstant.GITEE:
-			fmt.Println("gitee登录", RustGitee.RedirectUrl())
 			u.Redirect(RustGitee.RedirectUrl(), 302)
 			break
 		case RustConstant.GITHUP:
-			fmt.Println("githup登录", RustGitHup.RedirectUrl())
 			u.Redirect(RustGitHup.RedirectUrl(), 302)
 			break
 		default:
@@ -65,12 +55,7 @@ func (u *UserController) Login() {
 	}
 }
 
-// @Title 授权gitee
-// @Description Logs user into the system
-// @Param	username		query 	string	true		"The username for login"
-// @Param	password		query 	string	true		"The password for login"
-// @Success 200 {string} login success
-// @Failure 403 user not exist
+// OauthGitee  授权gitee
 func (u *UserController) OauthGitee() {
 	// 获取code
 	code := u.GetString("code")
@@ -88,62 +73,61 @@ func (u *UserController) OauthGitee() {
 	}
 	// 通过token 获取用户信息
 	user := RustGitee.GetUserInfos(userToken)
-	fmt.Println(user, "user---user")
 	bl = utils.IsEmpty(user.Id)
 	if !bl {
 		u.Fail("user.Id为空---系统错误", 500)
 		return
 	}
-	// 通过token 获取邮箱
-	emailsReust := RustGitee.GetEmails(userToken)
-	// 序列化  取邮箱
-	emailsAll := []map[string]interface{}{}
-	json.Unmarshal([]byte(emailsReust), &emailsAll)
+	//// 通过token 获取邮箱
+	//emailsReust := RustGitee.GetEmails(userToken)
+	//// 序列化  取邮箱
+	//emailsAll := []map[string]interface{}{}
+	//json.Unmarshal([]byte(emailsReust), &emailsAll)
+	//
+	//email := ""
+	//if len(emailsAll) != 0 {
+	//	email = emailsAll[0]["email"].(string)
+	//}
 
-	email := ""
-	if len(emailsAll) != 0 {
-		email = emailsAll[0]["email"].(string)
-	}
-	fmt.Println(email)
-
-	uid, _ := gonanoid.New()
-	UserAuth := models.UserAuth{
-		Uid:          uid,
-		IdentityType: 2,
-		Identifier:   strconv.Itoa(user.Id),
-		Certificate:  "",
-		CreateTime:   int(utils.GetUnix()),
-		UpdateTime:   int(utils.GetUnix()),
-	}
-	UserBase := models.UserBase{
-		Uid:            uid,
-		UserRole:       0,
-		RegisterSource: 2,
-		UserName:       user.Login,
-		NickName:       user.Name,
-		Gender:         0,
-		Birthday:       0,
-		Signature:      "",
-		Mobile:         "",
-		MobileBindTime: 0,
-		Email:          "",
-		EmailBindTime:  0,
-		Face:           user.AvatarUrl,
-		Face200:        "",
-		Srcface:        "",
-		CreateTime:     int(utils.GetUnix()),
-		UpdateTime:     int(utils.GetUnix()),
-	}
 	// 插入数据库
 	o := orm.NewOrm()
 	GetAuth := []models.UserAuth{}
 	num, err := o.Raw("select * from user_auth where identity_type = ? and identifier = ? limit 100", 2, user.Id).QueryRows(&GetAuth)
-	fmt.Println(num, "---------res", GetAuth)
 	if err != nil {
-		u.Fail("用户查询报错---系统错误", 500)
+		u.Fail("用户查询auth报错---系统错误", 500)
 		return
 	}
-	if num == 0 {
+
+	switch num {
+	case 0:
+		uid, _ := gonanoid.New()
+		UserAuth := models.UserAuth{
+			Uid:          uid,
+			IdentityType: 2,
+			Identifier:   strconv.Itoa(user.Id),
+			Certificate:  "",
+			CreateTime:   int(utils.GetUnix()),
+			UpdateTime:   int(utils.GetUnix()),
+		}
+		UserBase := models.UserBase{
+			Uid:            uid,
+			UserRole:       0,
+			RegisterSource: 2,
+			UserName:       user.Login,
+			NickName:       user.Name,
+			Gender:         0,
+			Birthday:       0,
+			Signature:      "",
+			Mobile:         "",
+			MobileBindTime: 0,
+			Email:          "",
+			EmailBindTime:  0,
+			Face:           user.AvatarUrl,
+			Face200:        "",
+			Srcface:        "",
+			CreateTime:     int(utils.GetUnix()),
+			UpdateTime:     int(utils.GetUnix()),
+		}
 		_, err = o.Insert(&UserBase)
 		if err != nil {
 			u.Fail("插入UserBase失败", 500)
@@ -154,86 +138,134 @@ func (u *UserController) OauthGitee() {
 			u.Fail("插入UserAuth失败", 500)
 			return
 		}
-		u.SetSession(uid, uid)
-		u.Ok("注册成功")
-	} else {
-		fmt.Println(uid, "uid---uid")
-		for i := 0; i < 100000; i++ {
-			u.SetSession(i, uid)
+		u.SetSession(uid, UserBase)
+		u.Ok(map[string]interface{}{
+			"msg":   "注册成功",
+			"token": uid,
+		})
+		break
+	case 1:
+		GetBase := []models.UserBase{}
+		num, err = o.Raw("select * from user_base where uid =?", GetAuth[0].Uid).QueryRows(&GetBase)
+		if err != nil {
+			u.Fail("用户查询base报错---系统错误", 500)
+			return
 		}
-		u.Ok("登录成功")
-	}
+		u.SetSession(GetBase[0].Uid, GetBase[0])
+		u.Ok(map[string]interface{}{
+			"msg":   "登录成功",
+			"token": GetBase[0].Uid,
+		})
 
-	//// 创建token 发放到cookie
-	//Secrect, _ := beego.AppConfig.String("Secrect")
-	//token, _ := utils.CreateToken(fmt.Sprintf("%d", user.Id), Secrect)
-	//u.Ctx.SetCookie("token", token)
-	//u.Redirect("/", 301)
+	default:
+		u.Fail("用户存在多个账号", 500)
+	}
 }
 
-// @Title 授权gitee
-// @Description Logs user into the system
-// @Param	username		query 	string	true		"The username for login"
-// @Param	password		query 	string	true		"The password for login"
-// @Success 200 {string} login success
-// @Failure 403 user not exist
+// OauthGitHup 授权githup
 func (u *UserController) OauthGitHup() {
 	// 获取code
 	code := u.GetString("code")
+	bl := utils.IsEmpty(code)
+	if !bl {
+		u.Fail("code为空---系统错误", 500)
+		return
+	}
 	// 通过code 获取token
 	userToken := RustGitHup.GetAccessToken(code)
+	bl = utils.IsEmpty(userToken.AccessToken)
+	if !bl {
+		u.Fail("userToken为空---系统错误", 500)
+		return
+	}
 	// 通过token 获取用户信息
 	user := RustGitHup.GetUserInfos(userToken)
-	// 通过token 获取邮箱
-	emailsReust := RustGitHup.GetEmails(userToken)
-	// 序列化  取邮箱
-	emailsAll := []map[string]interface{}{}
-	json.Unmarshal([]byte(emailsReust), &emailsAll)
-
-	email := ""
-	if len(emailsAll) != 0 {
-		email = emailsAll[0]["email"].(string)
+	bl = utils.IsEmpty(user.Id)
+	if !bl {
+		u.Fail("user.Id为空---系统错误", 500)
+		return
 	}
-	fmt.Println(email)
-
-	//o := orm.NewOrm()
-	//var maps []orm.Params
-	//r, _ := o.Raw("show tables").Values(&maps)
-	//fmt.Println(r, maps[0]["Tables_in_rust"])
-
-	u.Ok(user)
-
-	//// 登录的  uid 也就是  gitee的id  不能为空  为空代表 取消授权，取消登录
-	//if string(user.Id) == "" || user.Id == 0 {
-	//	u.Ctx.Redirect(301, "/")
-	//	return
-	//}
+	//// 通过token 获取邮箱
+	//emailsReust := RustGitHup.GetEmails(userToken)
+	//// 序列化  取邮箱
+	//emailsAll := []map[string]interface{}{}
+	//json.Unmarshal([]byte(emailsReust), &emailsAll)
 	//
-	//// 查询 数据库  是否有  用户存在
-	//Users := []models.User{}
-	//err := utils.DB.Raw("select * from user where uid = ?", user.Id).Find(&Users).Error
-	//if err != nil {
-	//	panic(err)
+	//email := ""
+	//if len(emailsAll) != 0 {
+	//	email = emailsAll[0]["email"].(string)
 	//}
-	//
-	//// 不存在 插入
-	//if len(Users) == 0 {
-	//	res := map[string]interface{}{}
-	//	err = utils.DB.Raw("insert into user (uid, username, login, email, avatar_url, html_url, created_at, status,addTime) VALUES (?,?,?,?,?,?,?,?)", user.Id, user.Name, user.Login, email, user.AvatarUrl, user.HtmlUrl, user.CreatedAt, utils.GetDate()).Find(&res).Error
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//} else {
-	//	// 存在  更新数据
-	//	res := map[string]interface{}{}
-	//	err = utils.DB.Raw("update user set username = ?,login=?,email=?,avatar_url=?,html_url=?,created_at=?,updateTime=? where uid = ?", user.Name, user.Login, email, user.AvatarUrl, user.HtmlUrl, user.CreatedAt, utils.GetDate(), user.Id).Find(&res).Error
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//}
-	//// 创建token 发放到cookie
-	//Secrect, _ := beego.AppConfig.String("Secrect")
-	//token, _ := utils.CreateToken(fmt.Sprintf("%d", user.Id), Secrect)
-	//u.Ctx.SetCookie("token", token)
-	//u.Redirect("/", 301)
+
+	// 插入数据库
+	o := orm.NewOrm()
+	GetAuth := []models.UserAuth{}
+	num, err := o.Raw("select * from user_auth where identity_type = ? and identifier = ? limit 100", 3, user.Id).QueryRows(&GetAuth)
+	if err != nil {
+		u.Fail("用户查询auth报错---系统错误", 500)
+		return
+	}
+
+	switch num {
+	case 0:
+		uid, _ := gonanoid.New()
+		UserAuth := models.UserAuth{
+			Uid:          uid,
+			IdentityType: 3,
+			Identifier:   strconv.Itoa(user.Id),
+			Certificate:  "",
+			CreateTime:   int(utils.GetUnix()),
+			UpdateTime:   int(utils.GetUnix()),
+		}
+		UserBase := models.UserBase{
+			Uid:            uid,
+			UserRole:       0,
+			RegisterSource: 3,
+			UserName:       user.Login,
+			NickName:       user.Name,
+			Gender:         0,
+			Birthday:       0,
+			Signature:      "",
+			Mobile:         "",
+			MobileBindTime: 0,
+			Email:          "",
+			EmailBindTime:  0,
+			Face:           user.AvatarUrl,
+			Face200:        "",
+			Srcface:        "",
+			CreateTime:     int(utils.GetUnix()),
+			UpdateTime:     int(utils.GetUnix()),
+		}
+		_, err = o.Insert(&UserBase)
+		if err != nil {
+			u.Fail("插入UserBase失败", 500)
+			return
+		}
+		_, err = o.Insert(&UserAuth)
+		if err != nil {
+			u.Fail("插入UserAuth失败", 500)
+			return
+		}
+		u.SetSession(uid, UserBase)
+		u.Ok(map[string]interface{}{
+			"msg":   "注册成功",
+			"token": uid,
+		})
+		break
+	case 1:
+		GetBase := []models.UserBase{}
+		num, err = o.Raw("select * from user_base where uid =?", GetAuth[0].Uid).QueryRows(&GetBase)
+		if err != nil {
+			u.Fail("用户查询base报错---系统错误", 500)
+			return
+		}
+		u.SetSession(GetBase[0].Uid, GetBase[0])
+		u.Ok(map[string]interface{}{
+			"msg":   "登录成功",
+			"token": GetBase[0].Uid,
+		})
+
+	default:
+		u.Fail("用户存在多个账号", 500)
+	}
+
 }
