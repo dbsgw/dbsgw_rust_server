b0988733782b1a3b77ea6c748035fb274f55fd92
坤元 <admin@dbsgw.cn>
2022-04-28 14:42:35 +0800

    中间件

---
+++ middleware/auth.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"fmt"
+	"github.com/beego/beego/v2/server/web/context"
+	"net/url"
+	"strings"
+)
+
+func Auth(ctx *context.Context) {
+	pathname := ctx.Request.URL.String()
+	urlPath, _ := url.Parse(pathname) //urlPath.Path  /role/edit
+
+	// 包含/admin路径的时候  就经行 验证token
+	StrContainers := strings.Contains(urlPath.Path, "/admin")
+	if StrContainers {
+		fmt.Println(1111)
+	}
+	fmt.Println("2222222222222------11111")
+	//if string(urlPath.Path) != "/admin/login" {
+	//	cookie := ctx.Input.Cookie("token")
+	//
+	//	Secrect, _ := beego.AppConfig.String("Secrect")
+	//	_, err := utils.ParseToken(cookie, Secrect)
+	//	if err != nil {
+	//		data := map[string]interface{}{
+	//			"code": 50014,
+	//			"data": false,
+	//			"msg":  "cookie登录失败",
+	//		}
+	//		//str, _ := json.Marshal(data)
+	//		//ctx.Output.Body(str)
+	//		fmt.Println(data)
+	//		ctx.SetCookie("token", "", -1)
+	//		ctx.Redirect(302, "/")
+	//	}
+	//}
+}
--- routers/router.go
+++ routers/router.go
@@ -9,12 +9,22 @@ package routers
 
 import (
 	"dbsgw_rust_server/controllers/v1"
-
+	"dbsgw_rust_server/middleware"
+	"fmt"
 	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/context"
 )
 
 func init() {
 	ns := beego.NewNamespace("/v1",
+		beego.NSCond(func(ctx *context.Context) bool {
+			fmt.Println(ctx.Input.Domain())
+			if ctx.Input.Domain() == "127.0.0.1" {
+				return true
+			}
+			return false
+		}),
+		beego.NSBefore(middleware.Auth),
 		// 登录/注册
 		beego.NSNamespace("/user",
 			beego.NSRouter("/login", &v1.UserController{}, "get:Login"),                    // 登录接口
